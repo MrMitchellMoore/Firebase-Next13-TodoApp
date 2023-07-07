b4bbfb3ea036659562d5b9c43ffb926037fe3342
Mitchell Moore <mmoorejr24@gmail.com>
2023-07-07 14:22:55 -0700

    fixed .gitignore

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    apiKey: "AIzaSyDgGUClhnwE4WJpdMWmq6i4MCpCycYKjtA",
-    authDomain: "todo-next-appdemo.firebaseapp.com",
-    projectId: "todo-next-appdemo",
-    storageBucket: "todo-next-appdemo.appspot.com",
-    messagingSenderId: "611519032520",
-    appId: "1:611519032520:web:1c13a041f070205717c9fd",
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
